cfdaab9a4db132975305ff6e322e85fe8d0ef76e
bybuss <kukuhaha729@gmail.com>
2023-10-24 19:26:04 +0300

    add PZ_4

--- PZ_4/PZ_4(1).py
+++ PZ_4/PZ_4(1).py
@@ -1,9 +1,11 @@
+# Дано целое число N (>0). Найти произведение 1.1 • 1.2 • 1.3 •... (N сомножителей).
+from decimal import Decimal
+
 n = int(input("Введите значение для n: "))
 
 multiplication = 1
 
-for i in range(n):
-    multiplication += 0.1
-    multiplication *= multiplication
+for i in range(n + 1):
+    multiplication *= Decimal("1") + Decimal(i) / Decimal("10")
 
 print(f"Произведение N сомножителей = {multiplication}")
