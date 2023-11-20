5b5241ac1f7af95e71820eac69b508c1df882823
bybuss <kukuhaha729@gmail.com>
2023-11-20 18:42:45 +0300

    commit

--- PZ_5/PZ_5(1).py
+++ PZ_5/PZ_5(1).py
@@ -1,12 +1,14 @@
 # Составить функцию, которая напечатает сорок любых символов.
 import random
 
+
 def forty_random_characters(lst):
     count = 0
 
     for i in range(40):
         count += 1
         print(f"{count}: {random.choice(lst)}")
+
 
 simbols = [
     'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
