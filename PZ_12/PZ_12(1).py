eb0ae3b1d76b8e47bfe9e36b9e0c81a1805cf547
bybuss <kukuhaha729@gmail.com>
2024-02-17 10:30:27 +0300

    17.02.24

--- PZ_12/PZ_12(1).py
+++ PZ_12/PZ_12(1).py
@@ -15,4 +15,8 @@ positive_list = [i for i in rnd_list if i > 0 and i % 2 == 0]
 print(f"Положительные и четные элементы: {positive_list}")
 
 print(f"Сумма: {sum(positive_list)}")
-print(f"Среднее арифметическое: {sum(positive_list) / len(positive_list)}")
+try:
+    print(f"Среднее арифметическое: {sum(positive_list) / len(positive_list)}")
+except ZeroDivisionError:
+    print(f"Из {N} произвольных чисел не были найдены положительные и четные элементы")
+
